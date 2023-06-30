034575f5d702616b54914b7bcaa0a132feebbc23
Vinicius <viniciusribeiro.dev@hotmail.com>
2023-06-30 15:51:24 -0300

    feat: view of user partial and UserSerializer

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from users.models import User
+from users.serializers import UserSerializer
 
 # Create your views here.
+
+class UserView(ListCreateAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+    
+
+class UserDetailview(RetrieveUpdateDestroyAPIView):
+  authentication_classes = [JWTAuthentication]
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+
+class LoginView(TokenObtainPairView):
+  pass
