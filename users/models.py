aafecbacb4273f8f9c2e9df954055803375e6702
Vinicius <viniciusribeiro.dev@hotmail.com>
2023-06-30 14:14:37 -0300

    fix: correction bugs in tests

--- users/models.py
+++ users/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    username = models.CharField(max_length=60)
+    username = models.CharField(max_length=60, unique=True)
     isAdm = models.BooleanField()
     email = models.EmailField(max_length=120, unique=True)
