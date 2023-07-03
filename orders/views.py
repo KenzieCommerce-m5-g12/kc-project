88d2a8bb291704be6b53381dc685ef0d18652d6a
Yuri Mota <yurirm55@gmail.com>
2023-07-03 19:46:56 -0300

    fix: correção da rota de order

--- orders/views.py
+++ orders/views.py
@@ -1,26 +1,48 @@
 from django.shortcuts import get_object_or_404
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from products.models import Product
 from .permissions import CanChangeOrderStatus
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.generics import *
 from orders.models import Orders
 from orders.serializers import OrdersSerializer
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework.views import Response, status
 
 
 class OrderListCreateView(ListCreateAPIView):
     queryset = Orders.objects.all()
     serializer_class = OrdersSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        products = self.request.data.get("products", [])
+        order = serializer.save(user=self.request.user)
+        if products:
+            for product_id in products:
+                product = get_object_or_404(Product, id=product_id)
+                order.products.add(product)
+            order.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class OrderRetrieveUpdateView(RetrieveUpdateAPIView):
+class OrderRetrieveUpdateView(RetrieveUpdateDestroyAPIView):
     queryset = Orders.objects.all()
     serializer_class = OrdersSerializer
+    authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated, CanChangeOrderStatus]
 
-    def perform_update(self, serializer):
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
         instance = self.get_object()
-        status = self.request.data.get("status")
-        instance.status = status
-        instance.save()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+
+        if "status" in serializer.validated_data:
+            instance.status = serializer.validated_data["status"]
+            instance.save()
+
+        self.perform_update(serializer)
+        return Response(serializer.data, status=status.HTTP_200_OK)
