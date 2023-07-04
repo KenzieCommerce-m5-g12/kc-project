92eb721caf7b8fd6c1d337d2c7697d8e11a1c7f1
Richard <richardpires663802@gmail.com>
2023-07-04 14:05:12 -0300

    add: cart view

--- cart/models.py
+++ cart/models.py
@@ -2,10 +2,26 @@ from django.db import models
 
 
 class Cart(models.Model):
-    products = models.ForeignKey(
-        "products.Product", on_delete=models.CASCADE, related_name="cart"
+    class Meta:
+        ordering = ["id"]
+
+    products_cart = models.ManyToManyField(
+        "products.Product", through="cart.CartProduct", related_name="cart_products"
     )
 
     user = models.OneToOneField(
         "users.User", on_delete=models.CASCADE, related_name="cart"
+    )
+
+
+class CartProduct(models.Model):
+    class Meta:
+        ordering = ["id"]
+
+    cart = models.ForeignKey(
+        "cart.Cart", on_delete=models.CASCADE, related_name="cart_data"
+    )
+
+    product = models.ForeignKey(
+        "products.Product", on_delete=models.CASCADE, related_name="product_data"
     )
