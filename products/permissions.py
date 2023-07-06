d868c4f24bd2e7c81c41830a083cfb1e6395f564
John Doe <julianosantosdev@outlook.com>
2023-07-06 14:12:10 -0300

    refactor: products permission detailsView, allow get/id, block patch and delete

--- products/permissions.py
+++ products/permissions.py
@@ -5,4 +5,7 @@ from products.models import Product
 
 class IsOwnerOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request: Request, view: View, obj) -> bool:
+        if request.method == "GET":
+            return True
+
         return obj.user == request.user or request.user.is_superuser
