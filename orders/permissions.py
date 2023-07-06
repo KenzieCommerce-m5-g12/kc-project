9c4ce78f958ec857e464e8954c69db4e7bd3a128
Yuri Mota <yurirm55@gmail.com>
2023-07-06 15:30:33 -0300

    fix: conserta seller

--- orders/permissions.py
+++ orders/permissions.py
@@ -1,12 +1,10 @@
 from rest_framework import permissions
-from rest_framework.views import View
-from .models import Orders
 
 
 class CanChangeOrderStatus(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         user = request.user
         if user.is_authenticated:
-            if user.typeUser == "Seller" or user.typeUser == "admin":
+            if user.typeUser == "seller" or user.typeUser == "admin":
                 return obj.user == user
         return False
