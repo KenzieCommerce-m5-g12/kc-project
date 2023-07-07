97a7e2a8fa04ec0f3f0b656f161fb00bc4140d09
John Doe <julianosantosdev@outlook.com>
2023-07-07 14:33:29 -0300

    update: deploy settings to render

--- kenzie_commerce/settings.py
+++ kenzie_commerce/settings.py
@@ -33,9 +33,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-RAILWAY_STATIC_URL = os.getenv("RAILWAY_STATIC_URL")
-if RAILWAY_STATIC_URL:
-    ALLOWED_HOSTS += [RAILWAY_STATIC_URL, "0.0.0.0"]
+RENDER_EXTERNAL_HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS +=[RENDER_EXTERNAL_HOSTNAME, "0.0.0.0"]
 
 
 # Application definition
@@ -111,6 +111,7 @@ if DATABASE_URL:
         default=DATABASE_URL, conn_max_age=500, ssl_require=True)
     DATABASES['default'].update(db_from_env)
     DEBUG = False
+
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
